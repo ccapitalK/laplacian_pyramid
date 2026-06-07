9fe74cd5e9070bd9773b71b1be38a1937170a840
Sahan Fernando <sahan.h.fernando@gmail.com>
2026-06-07 14:10:44 +1000

    Finish off pyramid tower

--- source/app.d
+++ source/app.d
@@ -12,18 +12,24 @@ struct Dims {
     size_t width, height;
 }
 
-float[2] coordsToO1(Dims dims, float x, float y) => [x / dims.width, y / dims.height];
-float[2] coordsFromO1(Dims dims, float x, float y) => [x * dims.width, y * dims.height];
+float[2] coordsToO1(float[2] c, Dims dims) => [c[0] / dims.width, c[1] / dims.height];
+float[2] coordsFromO1(float[2] c, Dims dims) => [c[0] * dims.width, c[1] * dims.height];
 
-Dims dims(in Image im) => Dims(im.width, im.height);
-Dims dims(in MirImage im) => Dims(im.length!0, im.length!1);
+float[2] vec2(size_t x, size_t y) => [cast(float) x, cast(float) y];
+float[2] vec2(float x, float y) => [x, y];
+
+Dims dims(in ref Image im) => Dims(im.width, im.height);
+Dims dims(in ref MirImage im) => Dims(im.length!0, im.length!1);
 
 Image image(in Dims dims) => Image(dims.width, dims.height);
 MirImage mirImage(in Dims dims) => slice!float(dims.width, dims.height, 3);
 
+size_t width(in ref MirImage im) => im.length!0;
+size_t height(in ref MirImage im) => im.length!1;
+
 alias MirImage = Slice!(float*, 3, Contiguous);
 
-Slice!(float*, 3, Contiguous) toNdSlice(Image im) {
+Slice!(float*, 3, Contiguous) toMirImage(Image im) {
     // TODO: SRGBify this
     // TODO: Double check the pitch/stride?
     auto dims = im.dims;
@@ -55,7 +61,9 @@ Image toImage(Slice!(float*, 3, Contiguous) mat) {
 }
 
 // Sufficient for upscaling, and for downscaling by a factor of at most 2, which is what we are doing
+// `x` should be in [0, mat.width), and `y` in [0, mat.height)
 float[3] bilerp(in MirImage mat, float x, float y) {
+    auto dims = mat.dims;
     float[3] v = [0, 0, 0];
     auto xI = cast(size_t) x.floor;
     auto yI = cast(size_t) y.floor;
@@ -66,13 +74,20 @@ float[3] bilerp(in MirImage mat, float x, float y) {
         size_t index;
         float weight;
     }
-    // FIXM: There is probably a more performant way of doing this. We need this to avoid OOB at the edge reads
-    Weight[2] xS = [Weight(xI, 1 - x), Weight(x > 0 ? xI + 1 : xI, x)];
-    Weight[2] yS = [Weight(yI, 1 - y), Weight(y > 0 ? yI + 1 : yI, y)];
+    // FIXME: This is actually subtly off, need to rethink what pixels as samples would mean here. Range being [) has
+    // introduced error here,
+    // FIXME: There is probably a more performant way of doing the clamp. We need this to avoid OOB at the edge reads
+    Weight[2] xS = [Weight(xI, 1 - x), Weight(xI + 1 < dims.width ? xI + 1 : xI, x)];
+    Weight[2] yS = [Weight(yI, 1 - y), Weight(yI + 1 < dims.height ? yI + 1 : yI, y)];
     foreach (xP; xS) {
         foreach (yP; yS) {
-            float weight = (0.25 * xP.weight * yP.weight);
-            v[] += mat[xP.index, yP.index].array[] * weight;
+            float weight = xP.weight * yP.weight;
+            if (!(xP.index < dims.width && yP.index < dims.height)) {
+                writefln!"%s %s %s"(dims, xP, yP);
+            }
+            enforce(xP.index < dims.width && yP.index < dims.height);
+            float[3] texel = mat[xP.index, yP.index].array[];
+            v[] += texel[] * weight;
         }
     }
     return v;
@@ -85,10 +100,9 @@ MirImage downscale2(in MirImage image) {
     auto halfDims = Dims(dims.width / 2, dims.height / 2);
     auto outImage = halfDims.mirImage;
     foreach (y; std.range.iota(0, halfDims.height).parallel) {
-        float vy = (y / cast (float) halfDims.height) * dims.height;
         foreach (x; 0 .. halfDims.width) {
-            float vx = (x / cast (float) halfDims.width) * dims.width;
-            outImage[x, y][] = image.bilerp(vx, vy);
+            auto c = vec2(x, y).coordsToO1(halfDims).coordsFromO1(dims);
+            outImage[x, y][] = image.bilerp(c[0], c[1]);
         }
     }
     return outImage;
@@ -99,47 +113,89 @@ MirImage upscale2(in MirImage image, Dims outDims) {
     auto dims = image.dims;
     auto outImage = outDims.mirImage;
     foreach (y; std.range.iota(0, outDims.height).parallel) {
-        float vy = (y / cast (float) outDims.height) * dims.height;
         foreach (x; 0 .. outDims.width) {
-            float vx = (x / cast (float) outDims.width) * dims.width;
-            outImage[x, y][] = image.bilerp(vx, vy);
+            auto c = vec2(x, y).coordsToO1(outDims).coordsFromO1(dims);
+            outImage[x, y][] = image.bilerp(c[0], c[1]);
+        }
+    }
+    return outImage;
+}
+
+MirImage add(MirImage a, MirImage b) {
+    enforce(a.dims == b.dims);
+    auto dims = a.dims;
+    auto outImage = dims.mirImage;
+    foreach (y; std.range.iota(0, dims.height).parallel) {
+        foreach (x; 0 .. dims.width) {
+            outImage[x, y][] = a[x, y] + b[x, y];
+        }
+    }
+    return outImage;
+}
+
+MirImage subtract(MirImage base, MirImage valToSubtract) {
+    enforce(base.dims == valToSubtract.dims);
+    auto dims = base.dims;
+    auto outImage = dims.mirImage;
+    foreach (y; std.range.iota(0, dims.height).parallel) {
+        foreach (x; 0 .. dims.width) {
+            outImage[x, y][] = base[x, y] - valToSubtract[x, y];
         }
     }
     return outImage;
 }
 
 struct LaplacianPyramid {
-    MirImage[] pyramid; // Index 0 is the lowest res as a delta from pitch black, the rest are deltas from the previous
+    MirImage[] levels; // Index 0 is the lowest res as a delta from pitch black, the rest are deltas from the previous
 }
 
 LaplacianPyramid makePyramid(MirImage image, int levels) {
     auto minSize = 4 << levels;
     enforce(image.dims.width >= minSize && image.dims.height >= minSize);
     LaplacianPyramid pyramid;
+    pyramid.levels ~= image;
+    foreach (i; 1 .. levels) {
+        auto last = pyramid.levels[$ - 1];
+        auto half = last.downscale2;
+        auto rec = half.upscale2(last.dims);
+        pyramid.levels[$ - 1] = last.subtract(rec);
+        pyramid.levels ~= half;
+    }
+    pyramid.levels.reverse();
     return pyramid;
 }
 
 MirImage reconstruct(LaplacianPyramid pyramid) {
-    // TODO
+    MirImage outImage = pyramid.levels[0];
+    foreach (i; 1 .. pyramid.levels.length) {
+        auto next = outImage.upscale2(pyramid.levels[i].dims);
+        outImage = next.add(pyramid.levels[i]);
+    }
+    return outImage;
 }
 
+alias BlendFunc = float delegate(float x, float y);
+
 LaplacianPyramid blend(LaplacianPyramid pyr1, LaplacianPyramid pyr2) {
-    enforce(pyr1.pyramid.length == pyr2.pyramid.length);
+    enforce(pyr1.levels.length == pyr2.levels.length);
+    // TODO
     return LaplacianPyramid();
 }
 
 Image blend(Image im1, Image im2, int nLevels = 6) {
     enforce(im1.width == im2.width && im1.height == im2.height);
-    auto pyr1 = im1.makePyramid(nLevels);
-    auto pyr2 = im2.makePyramid(nLevels);
-    auto blended = blend(im1, im2);
-    // return im1;
-    return blended.reconstruct;
+    auto pyr1 = im1.toMirImage.makePyramid(nLevels);
+    auto pyr2 = im2.toMirImage.makePyramid(nLevels);
+    auto blended = blend(pyr1, pyr1);
+    return blended.reconstruct.toImage;
 }
 
 void main(string[] args) {
     enforce(args.length == 3);
     auto im1 = args[1].loadImageRgb;
     auto im2 = args[2].loadImageRgb;
-    blend(im1, im2).writeImageRgb("out.bmp");
+    // blend(im1, im2).writeImageRgb("out.bmp");
+    auto pyr = im1.toMirImage.makePyramid(6);
+    pyr.levels[0].toImage.writeImageRgb("base.bmp");
+    pyr.reconstruct.toImage.writeImageRgb("out.bmp");
 }
